c258147132f6da7ff85c09f796840c596aae2d6c
magdasokolovic <magda.sokolovic@gmail.com>
2021-04-21 11:13:10 +0200

    Updates

---
+++ precache-manifest.95e47c38dbad600f3dd3f4858006feed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df775588b35d8827f067a68e82dd541",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bb1321302f2b5e4f9f5f",
+    "url": "./static/css/main.7e3f7e0d.chunk.css"
+  },
+  {
+    "revision": "735acf69baecd2304828",
+    "url": "./static/js/2.dad41058.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.dad41058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb1321302f2b5e4f9f5f",
+    "url": "./static/js/main.78a3816a.chunk.js"
+  },
+  {
+    "revision": "8cdb25f3ffb41440c4d8",
+    "url": "./static/js/runtime-main.e6c4d886.js"
+  }
+]);
